6308e1a284bd649c5ee9d0ec279668ab3a7c916c
christianLingue <christianklingue@gmail.com>
2022-05-21 05:49:44 +0100

    update UserDTO, add Guard, update environement config

--- dist/environement.js
+++ dist/environement.js
@@ -1,11 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.databaseURL = void 0;
+const config_1 = require("@nestjs/config");
+config_1.ConfigModule.forRoot();
 const config = {
-    mode: 'prod',
+    mode: 'dev',
 };
 let devDatabaseURL = 'mongodb://localhost:27017/bibliotheque-api';
 let prodDatabaseURL = process.env.DATABASE_CONNECTION;
-let databaseURL = config.mode = 'dev' ? devDatabaseURL : prodDatabaseURL;
+let databaseURL = config.mode == 'dev' ? devDatabaseURL : prodDatabaseURL;
 exports.databaseURL = databaseURL;
 //# sourceMappingURL=environement.js.map
--- environement.ts
+++ environement.ts
@@ -1,11 +1,15 @@
+import { ConfigModule } from '@nestjs/config';
+
+ConfigModule.forRoot();
+
 const config = {
-    mode: 'prod',
+    mode: 'dev',
 }
 
 let devDatabaseURL = 'mongodb://localhost:27017/bibliotheque-api';
 let prodDatabaseURL = process.env.DATABASE_CONNECTION;
 
-let databaseURL = config.mode = 'dev' ? devDatabaseURL : prodDatabaseURL;
+let databaseURL = config.mode == 'dev' ? devDatabaseURL : prodDatabaseURL;
 
 export {
     databaseURL
